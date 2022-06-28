a4e40aa69090b77501f1cc85b4b55af326647e47
yoojin <yoojin9005@gmail.com>
2022-06-28 12:50:08 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(play = "roller-skate") {
+    return `This Saturday, I want to ${play}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(special = "*") {
+    const inner = function (position = "a programmer") {
+        return `You are ${special}${position}${special}!`
+    }
+    return inner
+}
